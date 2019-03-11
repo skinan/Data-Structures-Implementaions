17d94882c720bd2a8f51c8a1fb4aef7a698a67bc
Sakib Khan Inan <46783803+skinan@users.noreply.github.com>
2019-03-11 19:38:07 +0600

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+
+#bubble sort
+#Getting Inputs
+x = input("Enter Elements: ").split(" ")
+
+#start of for loop to compare and interchange elements 
+
+for i in range(0, len(x)):
+    for j in range(0, len(x) - (i + int(1))):
+        y = x[j+1]
+        if int(x[j]) > int(x[j+1]):
+            x[j+1] = x[j]
+            x[j] = y
+
+#printing the list after sorting it in ascending order
+print("After Sorting : ", " ".join(x))
