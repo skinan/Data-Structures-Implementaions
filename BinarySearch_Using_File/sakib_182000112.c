bbdcafe2dff3cb84aa7d2dcd3f386be4c02ac254
Sakib Khan Inan <46783803+skinan@users.noreply.github.com>
2019-03-30 23:44:03 +0600

    Add files via upload

---
+++ BinarySearch_Using_File/sakib_182000112.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    int search;
+    FILE *fptr,*optr;
+
+    fptr = fopen("sakib.txt", "r");
+    optr = fopen("sakibK.txt","w");
+
+    fscanf(fptr,"%d", &n);
+
+    int arr[n];
+
+    for(i=0; i<n; i++)
+    {
+        fscanf(fptr,"%d", &arr[i]);
+    }
+
+
+
+        int f=0;
+            int l=n;
+                int m;
+    fscanf(fptr,"%d", &search);
+
+    while(f<=l)
+    {
+        m = (f + l)/2;
+        if(arr[m] == search)
+        {
+            fprintf(optr, "Match found at position %d\n", m + 1);
+            break;
+        }
+        else if(arr[m] < search)
+        {
+            f = m + 1;
+        }
+        else
+        {
+            l = m - 1;
+        }
+    }
+    if(f > l)
+    {
+        fprintf(optr, "No match found\n");
+    }
+
+
+    fclose(fptr);
+    fclose(optr);
+}
+
+
+
